a78e1a1a3a0e8263ccf0123992342aace57524fc
dinavalisoaa <randriamifidydina@gmail.com>
2024-02-01 23:19:16 +0300

    appointment service

---
+++ src/app/service/appointment/appointment.service.ts
@@ -0,0 +1,33 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AppointmentService {
+
+
+    constructor(private http: HttpClient) { }
+
+// getExpense(query:any ,next: (res: any) => any){
+
+//       this.http.get("http://localhost:5050/api/expense"+query).subscribe((res=>{
+//         next(res);
+//         close();
+//       }))
+//   }
+//   saveExpense(data:Expense,next: (res: any) => any){
+//     this.http.post("http://localhost:5050/api/expense",data).subscribe((res=>{
+//       next(res);
+//       close();
+//     }))
+// }
+// updateExpense(data:Expense,param:any, next: (res: any) => any){
+//     this.http.put("http://localhost:5050/api/expense/"+param,data).subscribe((res=>{
+//       next(res);
+//       close();
+//     }))
+
+// }
+
+}
