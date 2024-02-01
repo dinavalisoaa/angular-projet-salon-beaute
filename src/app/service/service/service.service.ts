a65fc659d7094fb02b432178c4a84f0d36a972a8
dinavalisoaa <randriamifidydina@gmail.com>
2024-02-01 23:19:52 +0300

    adding model Expense,Sex,Customer, and other

---
+++ src/app/service/service/service.service.ts
@@ -0,0 +1,33 @@
+ import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Service } from '../../models/models';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ServiceService {
+
+    constructor(private http: HttpClient) { }
+
+getService(query:any ,next: (res: any) => any){
+
+      this.http.get("http://localhost:5050/api/service"+query).subscribe((res=>{
+        next(res);
+        close();
+      }))
+  }
+  saveService(data:Service,next: (res: any) => any){
+    this.http.post("http://localhost:5050/api/service",data).subscribe((res=>{
+      next(res);
+      close();
+    }))
+}
+updateService(data:Service,param:any, next: (res: any) => any){
+    this.http.put("http://localhost:5050/api/service/"+param,data).subscribe((res=>{
+      next(res);
+      close();
+    }))
+
+}
+
+}
