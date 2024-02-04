0af9296c42261a43d1982db22a1a7568fa84b731
dinavalisoaa <randriamifidydina@gmail.com>
2024-02-05 00:23:05 +0300

    permission customer

--- src/app/demo/components/customer/customer-routing.module.ts
+++ src/app/demo/components/customer/customer-routing.module.ts
@@ -1,19 +1,53 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { AppLayoutComponent } from 'src/app/layout/app.layout.component';
+import { AuthGuardPermission } from '../manager/auth-permission';
 
 @NgModule({
-    imports: [RouterModule.forChild([
-        { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) },
-        { path: 'registration', loadChildren: () => import('./registration/registration.module').then(m => m.RegistrationModule) },
-        {
-            path: '', component: AppLayoutComponent,
-            children: [
-                { path: 'preference', loadChildren: () => import('./preference/preference.module').then(m => m.PreferenceModule) },
-                { path: 'appointment/making', loadChildren: () => import('./appointment/appointment.module').then(m => m.AppointmentModule) }
-            ],
-        }
-    ])],
-    exports: [RouterModule]
+    imports: [
+        RouterModule.forChild([
+            {
+                path: 'login',
+                loadChildren: () =>
+                    import('./login/login.module').then((m) => m.LoginModule),
+            },
+            {
+                path: 'registration',
+                loadChildren: () =>
+                    import('./registration/registration.module').then(
+                        (m) => m.RegistrationModule
+                    ),
+            },
+            {
+                path: '',
+                component: AppLayoutComponent,
+                children: [
+                    {
+                        path: 'preference',
+                        canActivate: [AuthGuardPermission],
+                        data: {
+                            role: 'CUSTOMER',
+                        },
+                        loadChildren: () =>
+                            import('./preference/preference.module').then(
+                                (m) => m.PreferenceModule
+                            ),
+                    },
+                    {
+                        path: 'appointment/making',
+                        canActivate: [AuthGuardPermission ],
+                        data: {
+                            role: 'CUSTOMER',
+                        },
+                        loadChildren: () =>
+                            import('./appointment/appointment.module').then(
+                                (m) => m.AppointmentModule
+                            ),
+                    },
+                ],
+            },
+        ]),
+    ],
+    exports: [RouterModule],
 })
-export class CustomerRoutingModule { }
+export class CustomerRoutingModule {}
