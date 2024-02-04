fe28da1e8bedfe87bd0d302ecaf717e3d30e2ab4
Nancia Rajerison <rajerisonnancia@gmail.com>
2024-02-04 22:42:42 +0300

    Update manager-routing.module.ts

--- src/app/demo/components/manager/manager-routing.module.ts
+++ src/app/demo/components/manager/manager-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { AppLayoutComponent } from 'src/app/layout/app.layout.component';
 
 @NgModule({
     imports: [RouterModule.forChild([
         { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) },
-        { path: 'service', loadChildren: () => import('./service-crud/service-crud.module').then(m => m.ServiceCrudModule) },
-        { path: 'expense', loadChildren: () => import('./expense/expense.module').then(m => m.ExpenseModule) }
+        {
+            path: '', component: AppLayoutComponent,
+            children: [
+                { path: 'service', loadChildren: () => import('./service-crud/service-crud.module').then(m => m.ServiceCrudModule) },
+                { path: 'expense', loadChildren: () => import('./expense/expense.module').then(m => m.ExpenseModule) },
+                { path: 'personnel', loadChildren: () => import('./personnel/personnel.module').then(m => m.PersonnelModule) }
+            ],
+        }
     ])],
     exports: [RouterModule]
 })
