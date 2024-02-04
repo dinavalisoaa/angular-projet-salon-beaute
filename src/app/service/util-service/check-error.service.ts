5e781b8c4baf6b5c802d86bd75de861319aceb8f
dinavalisoaa <randriamifidydina@gmail.com>
2024-02-05 00:07:47 +0300

    error handler service

---
+++ src/app/service/util-service/check-error.service.ts
@@ -0,0 +1,76 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { catchError, throwError } from 'rxjs';
+import { Manager } from 'src/app/models/models';
+
+@Injectable({
+    providedIn: 'root',
+})
+
+export const CheckError = (next: (res: any) => any) => {
+    return {
+      next: next,
+      error: (err: HttpError) => {
+        let errorData: SweetAlertOptions = {icon: "error", title: "Erreur",};
+        if (err.status === 0) {
+          errorData.text = "Êtes-vous connecté a internet ?";
+        }
+        else if (err.status === 403 || err.status === 401) {
+          errorData.text = "Veuillez vous connecter a un compte qui peut ouvrir cette Page !";
+        }
+        else if (500 - err.status <= 0) {
+          errorData.text = "Une erreur est survenue"
+        }
+        else {
+          errorData.text = err.error.error.message;
+        }
+        Swal.fire(errorData).then();
+      }
+    }
+  };
+
+export class CheckErrorService {
+
+    constructor(private http: HttpClient) {}
+    private handleError(error: HttpErrorResponse) {
+        if (error.status == 0) {
+        } else {
+            alert(error.error.error);
+        }
+        return throwError(() => new Error('eeeeeeeeeeee'));
+    }
+    getManager(query: any, next: (res: any) => any) {
+        this.http
+            .get('http://localhost:5050/api/mange' + query)
+            .subscribe((res) => {
+                next(res);
+                close();
+            });
+    }
+    saveManager(data: Manager, next: (res: any) => any) {
+        this.http
+            .post('http://localhost:5050/api/Manager', data)
+            .subscribe((res) => {
+                next(res);
+                close();
+            });
+    }
+
+    loginManager(data: Manager, next: (res: any) => any) {
+        this.http
+            .post('http://localhost:5050/api/manager/connection', data)
+
+            .subscribe((res) => {
+                next(res);
+                close();
+            });
+    }
+    updateManager(data: Manager, param: any, next: (res: any) => any) {
+        this.http
+            .put('http://localhost:5050/api/appointment/' + param, data)
+            .subscribe((res) => {
+                next(res);
+                close();
+            });
+    }
+}
