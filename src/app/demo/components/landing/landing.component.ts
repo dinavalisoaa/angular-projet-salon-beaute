65282c9c00cc12b9beccb6f528efe3e4d4891ea4
dinavalisoaa <randriamifidydina@gmail.com>
2024-02-13 22:24:28 +0300

    page d'accueil

--- src/app/demo/components/landing/landing.component.ts
+++ src/app/demo/components/landing/landing.component.ts
@@ -1,62 +1,133 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { LayoutService } from 'src/app/layout/service/app.layout.service';
+import { Appointment, Service } from 'src/app/models/models';
+import Swal from 'sweetalert2';
+import { ProductService } from '../../service/product.service';
+import { UtilService } from 'src/app/service/util-service/util.service';
+import { ServiceService } from 'src/app/service/service/service.service';
+import { AppointmentService } from 'src/app/service/appointment/appointment.service';
+import { AccountService } from 'src/app/service/account/account.service';
+import { MessageService } from 'primeng/api';
+import { CustomerService } from 'src/app/service/customer/customer.service';
+import { DataView } from 'primeng/dataview';
 
 @Component({
     selector: 'app-landing',
     templateUrl: './landing.component.html',
-    styles: [`
-        #hero{
-            background: linear-gradient(0deg, rgba(255, 255, 255, 0.2), rgba(255, 255, 255, 0.2)), radial-gradient(77.36% 256.97% at 77.36% 57.52%, #EEEFAF 0%, #C3E3FA 100%);
-            height:700px;
-            overflow:hidden;
-        }
-
-        .pricing-card:hover{
-            border:2px solid var(--cyan-200) !important;
-        }
-
-        @media screen and (min-width: 768px) {
-            #hero{
-                -webkit-clip-path: ellipse(150% 87% at 93% 13%);
-                clip-path: ellipse(150% 87% at 93% 13%);
-                height: 580px;
-            }
-        }
-
-        @media screen and (min-width: 1300px){
-            #hero > img {
-                position: absolute;
-                transform:scale(1.2);
-            }
-
-        #hero > div > p {
-                max-width: 450px;
-            }
-        }
-
-        @media screen and (max-width: 1300px){
+    styles: [
+        `
             #hero {
-                height: 600px;
+                background: linear-gradient(
+                        0deg,
+                        rgba(255, 255, 255, 0.2),
+                        rgba(255, 255, 255, 0.2)
+                    ),
+                    radial-gradient(
+                        77.36% 256.97% at 77.36% 57.52%,
+                        #eeefaf 0%,
+                        #c3e3fa 100%
+                    );
+                height: 700px;
+                overflow: hidden;
             }
 
-        #hero > img {
-            position:static;
-            transform: scale(1);
-            margin-left: auto;
-        }
-
-        #hero > div {
-            width: 100%;
-        }
-
-        #hero > div > p {
-                width: 100%;
-                max-width: 100%;
+            .pricing-card:hover {
+                border: 2px solid var(--cyan-200) !important;
             }
-        }
-    `]
+
+            @media screen and (min-width: 768px) {
+                #hero {
+                    -webkit-clip-path: ellipse(150% 87% at 93% 13%);
+                    clip-path: ellipse(150% 87% at 93% 13%);
+                    height: 580px;
+                }
+            }
+
+            @media screen and (min-width: 1300px) {
+                #hero > img {
+                    position: absolute;
+                    transform: scale(1.2);
+                }
+
+                #hero > div > p {
+                    max-width: 450px;
+                }
+            }
+
+            @media screen and (max-width: 1300px) {
+                #hero {
+                    height: 600px;
+                }
+
+                #hero > img {
+                    position: static;
+                    transform: scale(1);
+                    margin-left: auto;
+                }
+
+                #hero > div {
+                    width: 100%;
+                }
+
+                #hero > div > p {
+                    width: 100%;
+                    max-width: 100%;
+                }
+            }
+        `,
+    ],
 })
-export class LandingComponent {
-    constructor(public layoutService: LayoutService, public router: Router) { }
+export class LandingComponent implements OnInit {
+
+    appointment: Appointment = {};
+    filledAppointment: Appointment = {};
+
+    allServices: Service[] = [];
+
+    servicesToDo: Service[] = [];
+    // service:Service={};
+    click(service: Service) {
+        console.log('.............');
+        // Swal.fire(service.toString());
+    }
+    saveService(service: Service) {
+        if (!this.servicesToDo.includes(service)) {
+            this.servicesToDo.push(service);
+        }
+    }
+    trashService(service: Service) {
+        // Swal.fire(service.toString());
+        this.servicesToDo.filter((val, i) => val._id == service._id).pop();
+    }
+    saveAppointment() {
+        }
+    onFilter(dv: DataView, event: Event) {
+        dv.filter((event.target as HTMLInputElement).value);
+    }
+    constructor(
+        public layoutService: LayoutService,
+        public router: Router,
+        private productService: ProductService,
+        private utilService: UtilService,
+        private serviceService: ServiceService,
+        private appointmentService: AppointmentService,
+        private accountService: AccountService,
+        // private service: MessageService,
+        private customerService: CustomerService
+    ) {}
+    format(date: any) {
+        return this.utilService.toDateFr(date);
+    }
+    fetchService() {
+        this.serviceService.getService('', (res) => {
+            this.allServices = res;
+            console.log(this.allServices);
+        });
+    }
+    ngOnInit() {
+        this.fetchService();
+
+        this.servicesToDo = [];
+    }
 }
--- src/app/demo/components/landing/landing.module.ts
+++ src/app/demo/components/landing/landing.module.ts
@@ -7,6 +7,14 @@ import { DividerModule } from 'primeng/divider';
 import { ChartModule } from 'primeng/chart';
 import { PanelModule } from 'primeng/panel';
 import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { DataViewModule } from 'primeng/dataview';
+import { PickListModule } from 'primeng/picklist';
+import { OrderListModule } from 'primeng/orderlist';
+import { InputTextModule } from 'primeng/inputtext';
+import { DropdownModule } from 'primeng/dropdown';
+import { RatingModule } from 'primeng/rating';
+import { ChipModule } from 'primeng/chip';
 
 @NgModule({
     imports: [
@@ -16,6 +24,16 @@ import { ButtonModule } from 'primeng/button';
         StyleClassModule,
         ChartModule,
         PanelModule,
+        ButtonModule,
+        CardModule,
+        InputTextModule,
+        DataViewModule,
+        PickListModule,
+        OrderListModule,
+        ChipModule,
+        InputTextModule,
+        DropdownModule,
+        RatingModule,
         ButtonModule
     ],
     declarations: [LandingComponent]
