f79a00cdb2899e2f90dd7a8bdc331cc55c7398e4
Nancia Rajerison <rajerisonnancia@gmail.com>
2024-02-14 16:03:23 +0300

    Create notification.service.ts

---
+++ src/app/service/notification/notification.service.ts
@@ -0,0 +1,46 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { CheckError } from '../util-service/error';
+import { API_URL, UtilService } from '../util-service/util.service';
+import { BehaviorSubject, Observable, map } from 'rxjs';
+
+// const apiUrl = process.env.API_URL;
+const apiUrl = API_URL;
+
+@Injectable({
+    providedIn: 'root',
+})
+export class NotificationService {
+    // private newNotificationSubject = new BehaviorSubject<boolean>(false);
+
+    constructor(private http: HttpClient, public uService: UtilService) {}
+
+    getNotifications(userId: any, next: (res: any) => any) {
+        this.http.get(`${apiUrl}/api/notifications/user/${userId}`).subscribe(
+            CheckError((res) => {
+                next(res);
+                close();
+            })
+        );
+    }
+
+    async checkForNewNotifications(userId: any): Promise<boolean> {
+        try {
+            const res = await this.http.get<any>(`${apiUrl}/api/notifications/user/${userId}`)
+                .toPromise();
+            return res.new > 0;
+        } catch (error) {
+            console.error('Error checking for new notifications:', error);
+            return false;
+        }
+      }
+
+    markAllAsRead(userId: any, next: (res: any) => any) {
+        this.http.put(`${apiUrl}/api/notifications/user/${userId}/markAllAsRead`, null).subscribe(
+            CheckError((res) => {
+                next(res);
+                close();
+            })
+        );
+    }
+}
