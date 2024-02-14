f86dc5e6d2f66c11e57051b222e9c134a9764fb5
Nancia Rajerison <rajerisonnancia@gmail.com>
2024-02-14 16:03:32 +0300

    Create special-offer.service.ts

---
+++ src/app/service/special-offer/special-offer.service.ts
@@ -0,0 +1,34 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { catchError, throwError } from 'rxjs';
+import { Employee, TokenObject } from 'src/app/models/models';
+import { CheckError } from '../util-service/error';
+import { API_URL, UtilService } from '../util-service/util.service';
+
+// const apiUrl = process.env.API_URL;
+const apiUrl = API_URL;
+
+@Injectable({
+    providedIn: 'root',
+})
+export class SpecialOfferService {
+    constructor(private http: HttpClient, public uService: UtilService) {}
+
+    getSpecialOffers(next: (res: any) => any) {
+        this.http.get(`${apiUrl}/api/specialOffers`).subscribe(
+            CheckError((res) => {
+                next(res);
+                close();
+            })
+        );
+    }
+
+    saveSpecialOffer(data: any, next: (res: any) => any) {
+        this.http.post(`${apiUrl}/api/specialOffer`, data).subscribe(
+            CheckError((res) => {
+                next(res);
+                close();
+            })
+        );
+    }
+}
