f91895ff95c36512efb4e4a7c660f24456289d54
Nancia Rajerison <rajerisonnancia@gmail.com>
2024-02-14 16:02:24 +0300

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,21 +20,31 @@ export class AppComponent {
 
 
 import { Component, OnInit } from '@angular/core';
-import { PrimeNGConfig } from 'primeng/api';
+import { MessageService, PrimeNGConfig } from 'primeng/api';
 import { LayoutService } from './layout/service/app.layout.service';
+import { NotificationService } from './service/notification/notification.service';
+import { UtilService } from './service/util-service/util.service';
 
 @Component({
     selector: 'app-root',
-    templateUrl: './app.component.html'
+    templateUrl: './app.component.html',
+    providers: [MessageService]
 })
+
 export class AppComponent implements OnInit {
 
-    constructor(private primengConfig: PrimeNGConfig, private layoutService: LayoutService) { }
+    constructor(
+        private primengConfig: PrimeNGConfig,
+        private layoutService: LayoutService,
+        private messageService: MessageService,
+        private notificationService: NotificationService,
+        private utilService: UtilService
+    ) { }
 
     ngOnInit(): void {
         this.primengConfig.ripple = true;       //enables core ripple functionality
 		document.documentElement.style.fontSize = '14px';
-		
+
         //optional configuration with the default configuration
         this.layoutService.config = {
             ripple: false,                      //toggles ripple on and off
@@ -43,9 +53,27 @@ export class AppComponent implements OnInit {
             colorScheme: 'light',               //color scheme of the template, valid values are "light" and "dark"
             //theme: 'lara-light-indigo',         //default component theme for PrimeNG
 			theme: 'mdc-light-deeppurple',         //default component theme for PrimeNG
-			
+
             scale: 14                           //size of the body font size to scale the whole application
         };
+
+        setInterval(() => {
+            if(this.utilService.getToken().role == 'CUSTOMER'){
+                const userId = this.utilService.getToken().info._id;
+                this.notificationService.checkForNewNotifications(userId)
+                .then(hasNewNotifications => {
+                    if (hasNewNotifications) {
+                        console.log('Vous avez de nouvelles notifications !');
+                        this.messageService.add({
+                            severity: 'info',
+                            summary: 'Nouvelles notifications 🔔',
+                            detail: 'Vous avez de nouvelles notifications !',
+                            life: 10000,
+                        });
+                    }
+                });
+            }
+        }, 10000); // Vérifie toutes les 10 secondes
     }
 
 }
