02bf2718d47ac2eac82be9bc339d6e8c6905c186
dinavalisoaa <randriamifidydina@gmail.com>
2024-02-16 02:21:14 +0300

    Update account.service.ts

--- src/app/service/account/account.service.ts
+++ src/app/service/account/account.service.ts
@@ -1,17 +1,21 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Account, Customer } from 'src/app/models/models';
+import { Account, Appointment, Customer } from 'src/app/models/models';
 import { CheckError } from '../util-service/error';
 import { API_URL, UtilService } from '../util-service/util.service';
 import Swal from 'sweetalert2';
-const apiUrl =API_URL;
+import { AppointmentService } from '../appointment/appointment.service';
+const apiUrl = API_URL;
 
 @Injectable({
     providedIn: 'root',
 })
-
 export class AccountService {
-    constructor(private http: HttpClient, private uService: UtilService) {}
+    constructor(
+        private http: HttpClient,
+        private uService: UtilService,
+        private appoint: AppointmentService
+    ) {}
     // getState(val:number,id: string) {
 
     // }
@@ -32,14 +36,12 @@ export class AccountService {
             );
     }
     getAccountState(id: string, next: (res: any) => any) {
-        this.http
-            .get(`${apiUrl}/api/account/state?id=` + id)
-            .subscribe(
-                CheckError((res) => {
-                    next(res);
-                    close();
-                })
-            );
+        this.http.get(`${apiUrl}/api/account/state?id=` + id).subscribe(
+            CheckError((res) => {
+                next(res);
+                close();
+            })
+        );
     }
     saveAccount(data: Account, next: (res: any) => any) {
         console.log(data);
@@ -50,22 +52,40 @@ export class AccountService {
                 Swal.fire({
                     icon: 'success',
                     title: 'Message',
-                    text:res.message,
+                    text: res.message,
                     footer: '',
                 });
-                close();
 
+                close();
+            })
+        );
+    }
+    saveAccountTransaction(data: Account, appoint: Appointment, next: (res: any) => any) {
+        console.log(data);
+        this.http.post(`${apiUrl}/api/account`, data).subscribe(
+            CheckError((res) => {
+                next(res);
+                // Swal.showLoading(Swal.getDenyButton())
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Message',
+                    text: res.message,
+                    footer: '',
+                });
+                appoint.isPaid=true;
+
+                this.appoint.saveAppointment(appoint, (res) => {});
+                return res;
+                // close();
             })
         );
     }
     updateAccount(data: Account, param: any, next: (res: any) => any) {
-        this.http
-            .put(`${apiUrl}/api/account/` + param, data)
-            .subscribe(
-                CheckError((res) => {
-                    next(res);
-                    close();
-                })
-            );
+        this.http.put(`${apiUrl}/api/account/` + param, data).subscribe(
+            CheckError((res) => {
+                next(res);
+                close();
+            })
+        );
     }
 }
