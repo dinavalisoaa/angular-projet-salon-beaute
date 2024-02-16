b43ba65047ef54f4475fbae462db1858eaa1495a
dinavalisoaa <randriamifidydina@gmail.com>
2024-02-16 17:26:28 +0300

    Update profile.component.ts

--- src/app/demo/components/employee/profile/profile.component.ts
+++ src/app/demo/components/employee/profile/profile.component.ts
@@ -1,212 +1,144 @@
 import { Component, OnInit } from '@angular/core';
 import { MessageService } from 'primeng/api';
 import { Table } from 'primeng/table';
-import { Service } from 'src/app/models/models';
+import { CountryService } from 'src/app/demo/service/country.service';
+import { Employee, Service, Sex, TokenObject } from 'src/app/models/models';
+import { EmployeeService } from 'src/app/service/employee/employee.service';
 import { ServiceService } from 'src/app/service/service/service.service';
+import { SexService } from 'src/app/service/sex/sex.service';
+import { UtilService } from 'src/app/service/util-service/util.service';
+import Swal from 'sweetalert2';
 
 @Component({
     templateUrl: './profile.component.html',
     providers: [MessageService],
 })
 export class ProfileComponent implements OnInit {
-    serviceDialog: boolean = false;
+    countries: any[] = [];
 
-    filtreDialog: boolean = false;
+    emp: Employee = {};
 
-    deleteServiceDialog: boolean = false;
+    cities: any[];
 
-    deleteServicesDialog: boolean = false;
+    filteredCountries: any[] = [];
 
-    services: Service[] = [];
+    value1: any;
 
-    service: Service = {};
+    value2: any;
 
-    selectedServices: Service[] = [];
+    value3: any;
 
-    submitted: boolean = false;
+    value4: any;
+    label: string = 'Changer la photo';
+    labelChoose: string = 'Choisir';
+    value5: any;
 
-    cols: any[] = [];
+    value6: any;
 
-    statuses: any[] = [];
+    value7: any;
 
-    rowsPerPageOptions = [5, 10, 20];
+    value8: any;
 
+    value9: any;
+
+    value10: any;
+
+    value11: any;
+
+    value12: any;
+    sexItem: Sex[] = [];
+    entry: any;
+    exit: any;
+    getDate(date: any) {
+        return date.toString().split('T')[0];
+    }
     constructor(
-        private serviceService: ServiceService,
-        private messageService: MessageService
-    ) {}
-
-    ngOnInit() {
-        this.fetchList("");
-
-        // this.serviceService.getServices().then(data => );
-        // console.log(this.services)
-        this.cols = [
-            { field: 'service', header: 'Service' },
-            { field: 'price', header: 'Price' },
-            { field: 'category', header: 'Category' },
-            { field: 'rating', header: 'Reviews' },
-            { field: 'inventoryStatus', header: 'Status' },
-        ];
-
-        this.statuses = [
-            { label: 'INSTOCK', value: 'instock' },
-            { label: 'LOWSTOCK', value: 'lowstock' },
-            { label: 'OUTOFSTOCK', value: 'outofstock' },
+        private countryService: CountryService,
+        private uService: UtilService,
+        private sexService: SexService,
+        private empService: EmployeeService
+    ) {
+        this.cities = [
+            { name: 'New York', code: 'NY' },
+            { name: 'Rome', code: 'RM' },
+            { name: 'London', code: 'LDN' },
+            { name: 'Istanbul', code: 'IST' },
+            { name: 'Paris', code: 'PRS' },
         ];
     }
-    myUploader(event: any, service?: any) {
-        console.log('onUpload() START');
+    editempInformation(event: any) {
+        const id = this.getEmp()._id;
+        const name = this.emp.name;
+        const profile = this.emp.profile;
+        const firstname = this.emp.firstname;
+        const dateOfBirth = this.emp.dateOfBirth;
+        const sex = this.emp.sex;
+        const address = this.emp.address;
+        const phoneNumber = this.emp.phoneNumber;
+        const email = this.emp.email;
+        const password = this.emp.password;
+        const schedule = { entry: this.entry, exit: this.exit };
+        const data: Employee = {
+            name,
+            firstname,
+            profile,
+            dateOfBirth,
+            sex,
+            address,
+            phoneNumber,
+            email,
+            password,
+            schedule,
+        };
+        this.empService.updatePersonnel(data, id, () => {
+            Swal.fire({
+                icon: 'success',
+                title: 'Message',
+                text: 'Modication effectuer',
+                footer: '',
+            });
+            this.fetchEmp();
+        });
+        // this.editempDialog = false;
+        // this.emp = {};
+    }
+    fetchEmp(){
+        this.empService.getOneEmployee(this.getToken().userId, (res) => {
+            this.emp = res;
+            this.entry = this.uService.getTimeFromDate(
+                this.emp.schedule?.entry
+            );
+            this.exit = this.uService.getTimeFromDate(this.emp.schedule?.exit);
+        });
+    }
+    ngOnInit() {
+        this.sexService.getAllSexes((res) => {
+            this.sexItem = res;
+        });
+        this.fetchEmp();
+        this.countryService.getCountries().then((countries) => {
+            this.countries = countries;
+        });
+    }
+    getToken() {
+        const token: TokenObject = this.uService.getToken();
+        return token;
+    }
+    getEmp() {
+        const token: TokenObject = this.getToken();
+        let employee: Employee = {};
+        employee = token.info;
+        return employee;
+    }
 
+    myUploader(event: any, service?: any) {
         for (let file of event.files) {
             var reader = new FileReader();
             reader.readAsDataURL(file);
             reader.onload = function () {
-                //   console.log(reader.result);
-                service.illustration = reader.result;
-                //   =reader.result;
+                service.profile = reader.result;
+                console.log(service.profile);
             };
-            console.log('FILE TO BE UPLOADED: ', file);
-            //   this.uploadedFiles.push(file);
         }
-        // this.service.illustration=
-        this.messageService.add({
-            severity: 'info',
-            summary: 'File Uploaded',
-            detail: '',
-        });
-    }
-    openNew() {
-        this.service = {};
-        this.submitted = false;
-        this.serviceDialog = true;
-    }
-
-    deleteSelectedServices() {
-        this.deleteServicesDialog = true;
-    }
-
-    editService(service: Service) {
-        this.service = { ...service };
-        this.serviceDialog = true;
-    }
-
-    deleteService(service: Service) {
-        this.deleteServiceDialog = true;
-        this.service = { ...service };
-    }
-
-    confirmDeleteSelected() {
-        this.deleteServicesDialog = false;
-        this.services = this.services.filter(
-            (val) => !this.selectedServices.includes(val)
-        );
-        this.messageService.add({
-            severity: 'success',
-            summary: 'Successful',
-            detail: 'Services Deleted',
-            life: 3000,
-        });
-        this.selectedServices = [];
-    }
-
-    confirmDelete() {
-        this.deleteServiceDialog = false;
-        this.services = this.services.filter(
-            (val) => val._id !== this.service._id
-        );
-        this.messageService.add({
-            severity: 'success',
-            summary: 'Successful',
-            detail: 'Service Deleted',
-            life: 3000,
-        });
-        this.service = {};
-    }
-
-    hideDialog() {
-        this.serviceDialog = false;
-        this.submitted = false;
-    }
-    fetchList(query:string) {
-        this.serviceService.getService(query,(res) => {
-            this.services = res;
-        });
-    }
-    search(){
-        const commission = this.service.commission;
-        const duration = this.service.duration;
-        const name = this.service.name;
-        const price = this.service.price;
-        const illustration = this.service.illustration;
-    }
-    saveService() {
-        const commission = this.service.commission;
-        const duration = this.service.duration;
-        const name = this.service.name;
-        const price = this.service.price;
-        const illustration = this.service.illustration;
-        const data: Service = {
-            name,
-            price,
-            commission,
-            duration,
-            illustration,
-        };
-
-        if (this.service._id == undefined) {
-            this.submitted = true;
-            console.log(data);
-            this.serviceService.saveService(data, () => {
-                this.messageService.add({
-                    severity: 'success',
-                    summary: 'Successful',
-                    detail: 'Service Created',
-                    life: 3000,
-                });
-                this.fetchList("");
-            });
-        } else {
-            this.serviceService.updateService(data, this.service._id, () => {
-                this.messageService.add({
-                    severity: 'success',
-                    summary: 'Successful',
-                    detail: 'Service Updated',
-                    life: 3000,
-                });
-                this.fetchList("");
-            });
-        }
-        this.serviceDialog = false;
-        this.service = {};
-    }
-
-    findIndexById(id: string): number {
-        let index = -1;
-        for (let i = 0; i < this.services.length; i++) {
-            if (this.services[i]._id === id) {
-                index = i;
-                break;
-            }
-        }
-
-        return index;
-    }
-
-    createId(): string {
-        let id = '';
-        const chars =
-            'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-        for (let i = 0; i < 5; i++) {
-            id += chars.charAt(Math.floor(Math.random() * chars.length));
-        }
-        return id;
-    }
-
-    onGlobalFilter(table: Table, event: Event) {
-        table.filterGlobal(
-            (event.target as HTMLInputElement).value,
-            'contains'
-        );
     }
 }
