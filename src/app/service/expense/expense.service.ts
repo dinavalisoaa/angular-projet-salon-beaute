2728368ccc2c7b9d00c3121aa60d7ac1e03488d3
dinavalisoaa <randriamifidydina@gmail.com>
2024-02-16 18:52:10 +0300

    loading page

--- src/app/service/expense/expense.service.ts
+++ src/app/service/expense/expense.service.ts
@@ -3,6 +3,8 @@ import { Injectable } from '@angular/core';
 import { Expense } from 'src/app/models/models';
 import { API_URL } from '../util-service/util.service';
 import { CheckError } from '../util-service/error';
+import Swal from 'sweetalert2';
+import { loadPage } from '../util-service/load';
 const apiUrl = API_URL;
 
 @Injectable({
@@ -12,14 +14,17 @@ export class ExpenseService {
     constructor(private http: HttpClient) {}
 
     getExpense(query: any, next: (res: any) => any) {
+        loadPage();
         this.http.get(`${apiUrl}/api/expense` + query).subscribe(
             CheckError((res) => {
+                Swal.close();
                 next(res);
                 close();
             })
         );
     }
     saveExpense(data: Expense, next: (res: any) => any) {
+        loadPage();
         this.http.post(`${apiUrl}/api/expense`, data).subscribe(
             CheckError((res) => {
                 next(res);
@@ -28,6 +33,7 @@ export class ExpenseService {
         );
     }
     updateExpense(data: Expense, param: any, next: (res: any) => any) {
+        loadPage();
         this.http.put(`${apiUrl}/api/expense/` + param, data).subscribe(
             CheckError((res) => {
                 next(res);
