d2527e0602e8b7b98ea9bb9ae7442ac6ec566b79
dinavalisoaa <randriamifidydina@gmail.com>
2024-02-16 18:52:02 +0300

    Update service.service.ts

--- src/app/service/service/service.service.ts
+++ src/app/service/service/service.service.ts
@@ -4,20 +4,21 @@ import { Service } from '../../models/models';
 import { UtilService } from '../util-service/util.service';
 import { CheckError } from '../util-service/error';
 import { API_URL } from '../util-service/util.service';
+import { closeLoad, loadPage } from '../util-service/load';
 const apiUrl = API_URL;
 @Injectable({
     providedIn: 'root',
 })
 export class ServiceService {
     constructor(private http: HttpClient, private utilService: UtilService) {}
-
     getService(query: any, next: (res: any) => any) {
-        this.http
-            .get(`${apiUrl}/api/services` + query)
-            .subscribe(
+        loadPage();
+
+        this.http.get(`${apiUrl}/api/services` + query).subscribe(
             CheckError((res) => {
                 next(res);
                 close();
+                closeLoad();
             })
         );
     }
@@ -27,15 +28,21 @@ export class ServiceService {
     }
 
     saveService(data: any, next: (res: any) => any) {
+        loadPage();
+
         this.http.post(`${apiUrl}/api/service`, data).subscribe(
             CheckError((res) => {
                 next(res);
                 close();
+                closeLoad();
+
             })
         );
     }
 
     sumService(data: Service, next: (res: any) => any) {
+        loadPage();
+
         let head = new Headers();
         this.createHeader(head);
         this.http
@@ -44,20 +51,23 @@ export class ServiceService {
                 // headers: head,
             })
             .subscribe(
-            CheckError((res) => {
-                next(res);
-                close();
-            })
-        );
+                CheckError((res) => {
+                    next(res);
+                    close();
+                    closeLoad();
+                })
+            );
     }
 
     updateService(data: Service, param: any, next: (res: any) => any) {
-        this.http
-            .put('${apiUrl}/api/service/' + param, data)
-            .subscribe(
+        loadPage();
+
+        this.http.put(`${apiUrl}/api/service/` + param, data).subscribe(
             CheckError((res) => {
                 next(res);
                 close();
+                closeLoad();
+
             })
         );
     }
