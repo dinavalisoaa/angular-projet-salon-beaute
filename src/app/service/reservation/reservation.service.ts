20c59466701c49e981c1bd1f7e1451306ce54707
Nancia Rajerison <rajerisonnancia@gmail.com>
2024-02-17 09:57:04 +0300

    Reservation service

---
+++ src/app/service/reservation/reservation.service.ts
@@ -0,0 +1,35 @@
+import { DataView } from 'primeng/dataview';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, catchError, throwError } from 'rxjs';
+import { Customer, TokenObject } from 'src/app/models/models';
+import { CheckError } from '../util-service/error';
+import { API_URL, UtilService } from '../util-service/util.service';
+
+// const apiUrl = process.env.API_URL;
+const apiUrl = API_URL;
+
+@Injectable()
+export class ReservationService {
+
+    constructor(private http: HttpClient, public uService: UtilService) {}
+
+    getNumberPerDay(next: (res: any) => any) {
+        this.http.get(`${apiUrl}/api/dashboard/reservations/list/per/day`).subscribe(
+            CheckError((res) => {
+                next(res);
+                close();
+            })
+        );
+    }
+
+    getAllReservations(next: (res: any) => any) {
+        this.http.get(`${apiUrl}/api/dashboard/reservations/list`).subscribe(
+            CheckError((res) => {
+                next(res);
+                close();
+            })
+        );
+    }
+
+}
