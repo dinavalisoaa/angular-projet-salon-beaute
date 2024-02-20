2a9d7d0f94b10eb95e4d5cdcc697f9c97d2567d3
Nancia Rajerison <rajerisonnancia@gmail.com>
2024-02-20 13:54:20 +0300

    Revitalize signout action

--- src/app/layout/app.topbar.component.ts
+++ src/app/layout/app.topbar.component.ts
@@ -1,6 +1,7 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { MenuItem } from 'primeng/api';
 import { LayoutService } from "./service/app.layout.service";
+import { UtilService } from '../service/util-service/util.service';
 
 @Component({
     selector: 'app-topbar',
@@ -16,5 +17,13 @@ export class AppTopBarComponent {
 
     @ViewChild('topbarmenu') menu!: ElementRef;
 
-    constructor(public layoutService: LayoutService) { }
+    constructor(
+        public layoutService: LayoutService,
+        private utilService: UtilService
+    ) { }
+
+    signOut() {
+        localStorage.removeItem("sessionId");
+        this.utilService.navigateTo('/');
+    }
 }
