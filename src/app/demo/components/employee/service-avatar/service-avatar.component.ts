b9ca672fc2a5668cf56100bf28499412e603a5f0
dinavalisoaa <randriamifidydina@gmail.com>
2024-02-28 02:18:02 +0300

    fix avatar debordement card

--- src/app/demo/components/employee/service-avatar/service-avatar.component.ts
+++ src/app/demo/components/employee/service-avatar/service-avatar.component.ts
@@ -6,8 +6,29 @@ import { Service } from 'src/app/models/models';
     templateUrl: './service-avatar.component.html',
 })
 export class ServiceAvatarComponent implements OnInit {
-    @Input() services: Service[]| undefined =[];
+    @Input() services: Service[] | undefined = [];
+    illustrations: any[] | undefined = [];
+    autre: any = '';
     constructor() {}
 
-    ngOnInit(): void {}
+    // getCurrence(item: any, services: Service[] | undefined) {
+    //     let count = services?.reduce(
+    //         (acc, cur) => (cur.name == item ? ++acc : acc),
+    //         0
+    //     );
+    //     return count;
+    // }
+
+    getDistinct(array: Service[] | undefined) {
+        return [...new Set(array?.map((item, index) => item.illustration))];
+    }
+    ngOnInit(): void {
+        this.illustrations = this.getDistinct(this.services).filter(
+            (val, index) => index < 5
+        );
+        if (this.services != undefined) {
+            this.autre = (this.services?.length - 5)+'+';
+        }
+        // this.services=this.getDistinct(this.services);
+    }
 }
